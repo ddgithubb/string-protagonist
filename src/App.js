29fc881c0dbd83c439046886f205a98782d2c652
Daniel Dai <daniel.dai.email@gmail.com>
2023-11-17 21:04:40 -0500

    Init structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { StartPage } from './views/StartPage';
+import { Game } from './views/Game'; 
+
 function App() {
 
     useEffect(() => {
@@ -10,7 +12,7 @@ function App() {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<StartPage />} />
-                <Route path="/game" element={<GamePage />} />
+                <Route path="/game" element={<Game />} />
             </Routes>
         </BrowserRouter>
     );
